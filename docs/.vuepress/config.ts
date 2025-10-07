4b7cdfd68e7ec2c254e12d0bf06226dc9ede786e
kelude <ikelude@gmail.com>
2025-10-08 05:10:05 +0800

    wip

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,46 +1,40 @@
-import { viteBundler } from '@vuepress/bundler-vite'
-import { defaultTheme } from '@vuepress/theme-default'
-import { defineUserConfig } from 'vuepress'
-import { navbar, sidebar } from './configs/index'
-import { docsearchPlugin } from '@vuepress/plugin-docsearch'
+import { viteBundler } from "@vuepress/bundler-vite";
+import { defaultTheme } from "@vuepress/theme-default";
+import { defineUserConfig } from "vuepress";
+import { navbar, sidebar } from "./configs/index";
+import { docsearchPlugin } from "@vuepress/plugin-docsearch";
 
 export default defineUserConfig({
-  base: '/',
+  base: "/",
 
   locales: {
-    '/': {
-      lang: 'en-US',
-      title: 'VuePress',
-      description: 'Vue-powered Static Site Generator',
+    "/": {
+      lang: "en-US",
+      title: "VuePress",
+      description: "Vue-powered Static Site Generator",
     },
   },
 
   bundler: viteBundler(),
   theme: defaultTheme({
-    logo: '/images/hero.png',
-    repo: 'kelude/docs',
+    logo: "/images/hero.png",
+    repo: "kelude/docs",
 
     locales: {
-        '/': {
-            navbar: navbar,
-            sidebar: sidebar,
-        }
+      "/": {
+        navbar: navbar,
+        sidebar: sidebar,
+      },
     },
-    
   }),
 
-  markdown: {
-    code: {
-      lineNumbers: false,
-    },
-  },
+  markdown: {},
 
   plugins: [
     docsearchPlugin({
-      appId: 'VYR1K2T971',
-      apiKey: '2f4cf3aa07c2bd8e1c476dea1cda6d94',
-      indexName: 'kelude',
+      appId: "VYR1K2T971",
+      apiKey: "2f4cf3aa07c2bd8e1c476dea1cda6d94",
+      indexName: "kelude",
     }),
   ],
-
-})
+});
